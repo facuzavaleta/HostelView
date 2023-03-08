92ea262faf5c5b1263d89d3faccd2bd0738100a4
facuzavaleta89 <facuzavaleta89@gmail.com>
2023-03-07 19:19:08 -0500

    Modificado el registro para que sea unico

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import client_login, admin_login, client_register, admin_register
+from .views import client_login, admin_login, register
 
 app_name="users"
 
 urlpatterns = [
     path('client/login', client_login, name='client_login'),
-    path('client/register', client_register, name='client_register'),
     path('admin/login', admin_login, name='admin_login'),
-    path('admin/register', admin_register, name='admin_register'),
+    path('register', register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,11 +4,8 @@ from django.shortcuts import render
 def client_login(request):
     return render(request, 'users/client_login.html')
 
-def client_register(request):
-    return render(request, 'users/client_register.html')
-
 def admin_login(request):
     return render(request, 'users/admin_login.html')
 
-def admin_register(request):
-    return render(request, 'users/admin_register.html')
+def register(request):
+    return render(request, 'users/register.html')
