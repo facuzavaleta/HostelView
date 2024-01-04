79728126542114af9883c4dab11d85911a71b97f
facuzavaleta <facuzavaleta890@gmail.com>
2024-01-03 23:19:35 -0300

    creada landing page, listo para empezar

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from pages.views import landing_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    path('', landing_view, name='landing'),
 ]
